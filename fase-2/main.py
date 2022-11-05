157e4d961b1f894b25610f83a59078c5360c2f1c
fran-janela <francisco.pinheiro.janela@gmail.com>
2022-11-05 18:37:53 -0300

    delete mov + correcao update mov
    Co-authored-by: Henrique Martinelli Frezzatti <henriquemf@users.noreply.github.com>

--- fase-2/main.py
+++ fase-2/main.py
@@ -20,26 +20,28 @@ def get_db():
     finally:
         db.close()
 
-#---------------------------------------------------------------------------------------------------------
-#                                               INVENTORY
-#---------------------------------------------------------------------------------------------------------
-
-
 #-----------------------------------STARTING PAGE----------------------------------#
 
 @app.get("/")
 def read_root():
     return {"Página Inicial": "Bem vindo a API de estoque, caminhos disponíveis: '/inventory' para GET e POST , '/inventory/{product_id}' para GET, PUT, PATCH e DELETE"}
 
+
+#---------------------------------------------------------------------------------------------------------
+#                                               INVENTORY
+#---------------------------------------------------------------------------------------------------------
+
+
+
 #---------------------------------GET ALL PRODUCTS---------------------------------#
 
-@app.get("/inventory")
+@app.get("/inventory", tags=["Inventory"])
 async def get_inventory(db: Session = Depends(get_db)):
     return db.query(models.Product).all()
 
 #--------------------------------GET BY SPECIFIC ID--------------------------------#
 
-@app.get("/inventory/{product_id}", response_model=schemas.Product)
+@app.get("/inventory/{product_id}", response_model=schemas.Product, tags=["Inventory"])
 async def get_product(
     product_id: int = Query(
         title="Product ID",
@@ -57,7 +59,7 @@ async def get_product(
 
 #--------------------------------CREATE NEW PRODUCT--------------------------------#
 
-@app.post("/inventory/", response_model=schemas.Product, status_code=status.HTTP_201_CREATED)
+@app.post("/inventory/", response_model=schemas.Product, status_code=status.HTTP_201_CREATED, tags=["Inventory"])
 async def create_product(
     product: schemas.ProductCreate = Body(
         example={
@@ -79,7 +81,7 @@ async def create_product(
 #----------------------------------UPDATE PRODUCT----------------------------------#
 
 ### PUT ###
-@app.put("/inventory/{product_id}", response_model=schemas.Product)
+@app.put("/inventory/{product_id}", response_model=schemas.Product, tags=["Inventory"])
 async def update_product_by_put(
     product_id: int = Query(
         title="Product ID",
@@ -106,7 +108,7 @@ async def update_product_by_put(
 
 
 ### PATCH ###
-@app.patch("/inventory/{product_id}", response_model=schemas.Product)
+@app.patch("/inventory/{product_id}", response_model=schemas.Product, tags=["Inventory"])
 async def update_product_by_patch(
     product_id: int = Query(
         title="Product ID",
@@ -144,7 +146,7 @@ async def update_product_by_patch(
     
 #----------------------------------DELETE PRODUCT----------------------------------#
 
-@app.delete("/inventory/{product_id}")
+@app.delete("/inventory/{product_id}", tags=["Inventory"])
 async def delete_product(
     product_id: int = Query(
         title="Product ID",
@@ -165,13 +167,13 @@ async def delete_product(
 
 #------------------------------------GET ALL MOVS----------------------------------#
 
-@app.get("/movement")
+@app.get("/movement", tags=["Movement"])
 async def get_movement(db: Session = Depends(get_db)):
     return db.query(models.Movement).all()
 
 #-------------------------------GET MOVEMENT BY SPECIFIC ID-----------------------------#
 
-@app.get("/movement/{id_mov}", response_model=schemas.Movement)
+@app.get("/movement/{id_mov}", response_model=schemas.Movement, tags=["Movement"])
 async def get_movement_by_id(
     id_mov: int = Query(
         title="Movement ID",
@@ -186,7 +188,7 @@ async def get_movement_by_id(
 
 #----------------------------------CREATE NEW MOVEMENT----------------------------------#
 
-@app.post("/movement/", response_model=schemas.Movement, status_code=status.HTTP_201_CREATED)
+@app.post("/movement/", response_model=schemas.Movement, status_code=status.HTTP_201_CREATED, tags=["Movement"])
 async def create_movement(
     mov: schemas.MovementCreate = Body(
         example={
@@ -217,7 +219,7 @@ async def create_movement(
 #----------------------------------UPDATE MOVEMENT----------------------------------#
 
 ### PATCH ###
-@app.patch("/movement/{id_mov}", response_model=schemas.Movement)
+@app.patch("/movement/{id_mov}", response_model=schemas.Movement, tags=["Movement"])
 async def update_movement_by_patch(
     id_mov: int = Query(
         title="Movement ID",
@@ -246,13 +248,19 @@ async def update_movement_by_patch(
         db_product_new_mov = db.query(models.Product).filter(models.Product.id == id_product).first()
         if db_product_new_mov is None:
             raise HTTPException(status_code=400, detail="Product ID not valid")
+        if quantity_change != None:
+            new_quantity_change = quantity_change
+        else:
+            new_quantity_change = db_mov.quantity_change
+        if db_product_old_mov.quantity - db_mov.quantity_change < 0:
+            raise HTTPException(status_code=400, detail="Old movement can't be reversed")
+        if db_product_old_mov.quantity - new_quantity_change < 0:
+            raise HTTPException(status_code=400, detail="Can't apply movement because final product quantity is less than 0")
         try:
             db_product_old_mov.quantity -= db_mov.quantity_change
+            db_product_new_mov.quantity += new_quantity_change
             if quantity_change != None:
-                db_product_new_mov.quantity += quantity_change
                 db_mov.quantity_change = quantity_change
-            else:
-                db_product_new_mov.quantity += db_mov.quantity_change
             db_mov.id_product = id_product
             db.commit()
             db.refresh(db_mov)
@@ -273,3 +281,50 @@ async def update_movement_by_patch(
             db.rollback()
     
     return db_mov
+
+#----------------------------------DELETE MOVEMENT----------------------------------#
+
+@app.delete("/movement/{id_mov}", tags=["Movement"])
+async def delete_movement(
+    id_mov: int = Query(
+        title="Movement ID",
+        description="Select the movement by it's ID (needs to be the lastest done to the product)"
+    ),
+    db: Session = Depends(get_db)
+):
+    db_mov = db.query(models.Movement).filter(models.Movement.id == id_mov).first()
+    if db_mov is None:
+        raise HTTPException(status_code=400, detail="Movement ID not valid")
+    db_product = db.query(models.Product).filter(models.Product.id == db_mov.id_product).first()
+    db_product_last_mov = db.query(models.Movement).filter(models.Product.id == db_mov.id_product).order_by(models.Movement.id.desc()).first()
+    if db_mov.id != db_product_last_mov.id:
+        raise HTTPException(status_code=400, detail="This movement is not the last one made to this product, please delete in order from last to first.")
+    try:
+        db_product.quantity -= db_mov.quantity_change
+        db.delete(db_mov)
+        db.commit()
+    except:
+        db.rollback()
+    return {"message": "Movement deleted successfully"}
+
+@app.delete("/movement/product/{id_product}", tags=["Movement by Product"])
+async def delete_last_movement_by_product(
+    id_product: int = Query(
+        title="Product ID",
+        description="Select the product by it's ID (it will delete the last moviment done to the product)"
+    ),
+    db: Session = Depends(get_db)
+):
+    db_product = db.query(models.Product).filter(models.Product.id == id_product).first()
+    if db_product is None:
+        raise HTTPException(status_code=400, detail="Product ID not valid")
+    db_product_last_mov = db.query(models.Movement).filter(models.Product.id == id_product).order_by(models.Movement.id.desc()).first()
+    if db_product_last_mov is None:
+        raise HTTPException(status_code=400, detail="Product has no movements")
+    try:
+        db_product.quantity -= db_product_last_mov.quantity_change
+        db.delete(db_product_last_mov)
+        db.commit()
+    except:
+        db.rollback()
+    return {"message": "Movement deleted successfully"}
